fa0097e8e8b2347bacb8ca161c7ef4a5ac7d0016
Duppils <anton.duppils@live.com>
2018-12-03 11:42:59 +0100

    datasets uncommented method

--- lab4/datasets.py
+++ lab4/datasets.py
@@ -2,19 +2,19 @@
 from conll_dictorizer import CoNLLDictorizer, Token
 
 
-#def load_conll2009_pos():
- #   train_file = '../../../corpus/conll2009/en/CoNLL2009-ST-English-train-pos.txt'
-  #  dev_file = '../../../corpus/conll2009/en/CoNLL2009-ST-English-development-pos.txt'
-   # test_file = '../../../corpus/conll2009/en/CoNLL2009-ST-test-words-pos.txt'
-    #test2_file = 'simple_pos_test.txt'
-#
- #   column_names = ['id', 'form', 'lemma', 'plemma', 'pos', 'ppos']
-#
- #   train_sentences = open(train_file).read().strip()
-  #  dev_sentences = open(dev_file).read().strip()
-   # test_sentences = open(test_file).read().strip()
-    #test2_sentences = open(test2_file).read().strip()
-    #return train_sentences, dev_sentences, test_sentences, column_names
+def load_conll2009_pos():
+    train_file = '../../../corpus/conll2009/en/CoNLL2009-ST-English-train-pos.txt'
+    dev_file = '../../../corpus/conll2009/en/CoNLL2009-ST-English-development-pos.txt'
+    test_file = '../../../corpus/conll2009/en/CoNLL2009-ST-test-words-pos.txt'
+    test2_file = 'simple_pos_test.txt'
+
+    column_names = ['id', 'form', 'lemma', 'plemma', 'pos', 'ppos']
+
+    train_sentences = open(train_file).read().strip()
+    dev_sentences = open(dev_file).read().strip()
+    test_sentences = open(test_file).read().strip()
+    test2_sentences = open(test2_file).read().strip()
+    return train_sentences, dev_sentences, test_sentences, column_names
 
 
 path = str(open("path.conf", "r").read()).rstrip('\n')
