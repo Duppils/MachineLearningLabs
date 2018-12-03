3928495189c88b2c84cf74dd20ed2360b415cd32
Duppils <anton.duppils@live.com>
2018-12-03 11:16:41 +0100

    remove conllu from git and update path in datasets

--- lab4/datasets.py
+++ lab4/datasets.py
@@ -2,28 +2,30 @@
 from conll_dictorizer import CoNLLDictorizer, Token
 
 
-def load_conll2009_pos():
-    train_file = '../../../corpus/conll2009/en/CoNLL2009-ST-English-train-pos.txt'
-    dev_file = '../../../corpus/conll2009/en/CoNLL2009-ST-English-development-pos.txt'
-    test_file = '../../../corpus/conll2009/en/CoNLL2009-ST-test-words-pos.txt'
-    test2_file = 'simple_pos_test.txt'
+#def load_conll2009_pos():
+ #   train_file = '../../../corpus/conll2009/en/CoNLL2009-ST-English-train-pos.txt'
+  #  dev_file = '../../../corpus/conll2009/en/CoNLL2009-ST-English-development-pos.txt'
+   # test_file = '../../../corpus/conll2009/en/CoNLL2009-ST-test-words-pos.txt'
+    #test2_file = 'simple_pos_test.txt'
+#
+ #   column_names = ['id', 'form', 'lemma', 'plemma', 'pos', 'ppos']
+#
+ #   train_sentences = open(train_file).read().strip()
+  #  dev_sentences = open(dev_file).read().strip()
+   # test_sentences = open(test_file).read().strip()
+    #test2_sentences = open(test2_file).read().strip()
+    #return train_sentences, dev_sentences, test_sentences, column_names
 
-    column_names = ['id', 'form', 'lemma', 'plemma', 'pos', 'ppos']
 
-    train_sentences = open(train_file).read().strip()
-    dev_sentences = open(dev_file).read().strip()
-    test_sentences = open(test_file).read().strip()
-    test2_sentences = open(test2_file).read().strip()
-    return train_sentences, dev_sentences, test_sentences, column_names
+path = str(open("path.conf", "r").read()).rstrip('\n')
 
-
-BASE_DIR = '/Users/pierre/Projets/Corpora/CoNLL2003/'
+BASE_DIR = path;
 
 
 def load_conll2003_en():
-    train_file = BASE_DIR + 'NER-data/eng.train'
-    dev_file = BASE_DIR + 'NER-data/eng.valid'
-    test_file = BASE_DIR + 'NER-data/eng.test'
+    train_file = BASE_DIR + 'corpus/eng.train'
+    dev_file = BASE_DIR + 'corpus/eng.valid'
+    test_file = BASE_DIR + 'corpus/eng.test'
     column_names = ['form', 'ppos', 'pchunk', 'ner']
     train_sentences = open(train_file).read().strip()
     dev_sentences = open(dev_file).read().strip()
